3e79ea7497df44191157fc946116dee3ae7bdf49
Nikhil Bharadwaj <nikhil.bharadwaj-s-m@hpe.com>
2017-03-27 20:41:46 +0530

    final

--- Trackify Chrome Plugin/controller.js
+++ Trackify Chrome Plugin/controller.js
@@ -1,27 +1,25 @@
-function getMongoServiceUrl() {
+function getServiceUrl() {
     //Replace it with the Mongo DB API URL
-	return "http://localhost:1337/";
-}
-
-function getDocumentServiceUrl() {
-    //Replace it with the Document DB API URL
-	return "http://localhost:3000/";
+	// return "http://localhost:9000/";
+    return "http://talenttrack.azurewebsites.net/";
 }
 
 angular.module('naukriplugin', []).controller('naukripluginctrl', function($scope, $http) {
     
     //Service call to get the list of collections
-    $http.get(getMongoServiceUrl() + "api/getAllActiveJobs").then(function (response) {
-        $scope.collections = response.data;            	
+    $http.get(getServiceUrl() + "api/jobs/getAllActiveJobs").then(function (response) {
+        $scope.collections = response.data.data;            	
     }, function (err) {
         alert(err.status);
     });
 
     //Service call to add or update documents in a collection
     $scope.addToCollection = function(collection) {
-        $http.post(getDocumentServiceUrl() + "add-candidate?collectionid=" + collection, window.candidate).then(function (response) {
-            if (response.data.status == 'ADD_SUCCESS' || response.data.status == 'UPDATE_SUCCESS') {
-                $scope.message = response.data.message;
+        var obj = window.candidate;
+        obj.jobId = parseInt(collection);
+        $http.post(getServiceUrl() + "api/jobs/add-candidate", obj).then(function (response) {
+            if (response.data.status == 'SUCCESS') {
+                $scope.message = response.data.message || response.data.err;
             }                     	
         }, function (err) {
             alert(err.status);
